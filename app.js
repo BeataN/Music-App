a4a08922c0b2d59fd35d0067aa15227163de3758
Becia <becia@MacBook-Air-Becia.local>
2016-07-16 12:22:28 +0100

    yt search module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     app.init();
+    appYT.init();
 });
 
 var app = {
@@ -8,16 +9,16 @@ var app = {
   },
   searchModule : {
     init : function(){
-      app.searchModule.prepareEvents(); // app????????????????
+      app.searchModule.prepareEvents();
     },
     prepareEvents : function(){
-      $("#search-field").on("keypress", function(e){
+      /*$("#search-field").on("keypress", function(e){
         var songName = $("#search-field").val();
         if(e.which === 13){
           app.searchModule.getSongs(songName);
           $('#main-search').addClass('moved-to-top');
         }
-      });
+      });*/
 
       $(".btn-search-songs").on("click", function(e){
         var songName = $("#search-field").val();
@@ -25,12 +26,12 @@ var app = {
         app.searchModule.getSongs(songName);
       });
     },
-    getSongs : function(songName){//?????????????
+    getSongs : function(songName){
       $.ajax({
           url: "https://api.spotify.com/v1/search",
           data: {
-            type : 'track', // ???????????
-            q: songName //????????????????
+            type : 'track',
+            q: songName
           },
           success: function(result){
             app.songsListModule.showSongs(result);
@@ -62,80 +63,46 @@ var app = {
 
     }
   }
-}
+},
 
-$(document).ready(function(){
-    app.init();
-});
-
-var app = {
+var appYT = {
   init : function(){
-    app.searchModule.init();
+    appYT.searchModuleYouTube.init();
   },
-  searchModule : {
-    init : function(){
-      app.searchModule.prepareEvents(); // app????????????????
-    },
-    prepareEvents : function(){
-      $("#search-field").on("keypress", function(e){
-        var songName = $("#search-field").val();
-        if(e.which === 13){
-          app.searchModule.getSongs(songName);
-          $('#main-search').addClass('moved-to-top');
-        }
-      });
-
-      $(".btn-search-songs").on("click", function(e){
-        var songName = $("#search-field").val();
-        $('#main-search').addClass('moved-to-top');
-        app.searchModule.getSongs(songName);
-      });
-    },
-    getSongs : function(songName){//?????????????
-      $.ajax({
-          url: "https://api.spotify.com/v1/search",
-          data: {
-            type : 'track', // ???????????
-            q: songName //????????????????
-          },
-          success: function(result){
-            app.songsListModule.showSongs(result);
-
-
-
-          },
-          error: function(result){
-            console.log('error=', result)
-          }
-
-        });
-    }
+searchModuleYouTube : {
+  init : function(){
+    appYT.searchModuleYouTube.prepareEvents();
   },
-
-  songsListModule : {
-    showSongs : function(result){
-      $("#tracks-list li").remove();
-
-      $.each(result.tracks.items, function(i,track){ //????????????????????? track/s
-        console.log(track);
-        var nameHtml = '<p class = "generated-song">' + track.name + '</p>';
-        var imgHtml = '<img class = "track-cover" src="' + track.album.images[0].url + '"/>';
-        var artistHtml = '<p class = "generated-artist">' + track.artists[0].name +  '</p>';
-        var previewPlayer = '<audio controls class = "audio-play"><source src="' + track.preview_url + '" type="audio/mpeg"></audio>';
-        $("#tracks-list").append('<li>' + imgHtml + '<div class="track-details">' + artistHtml +' '+ '-'+ ' '+nameHtml + previewPlayer + '</div></li>');
-        $('#tracks-list').addClass('moved-to-top-ul');
-      })
-
-    }
-  }
-}
-
-//you tube
-
-//https://api.spotify.com/v1/search
-//przesuniecie napisu, lista li, empty search field
-
-
-/*  $(".btn-search-songs").click(function(){
+prepareEvents : function(){
+  $(".btn-search-songs-yt").on("click", function(e){
     var songName = $("#search-field").val();
-    app.searchModule.getSongs(songName); */
+    $('#main-search').addClass('moved-to-top');
+    appYT.searchModuleYouTube.getYouTubeSongs(songName);
+  });
+},
+getYouTubeSongs : function(songName){
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/search",
+        data: {
+          part: 'snippet',
+          key: 'AIzaSyDRiHqtrdxrs9wiMPUr595ys9h6CJwFwiY',
+          q: songName
+        },
+        success: function(result){
+          appYT.songsListModule.showSongs(result);
+
+
+
+        },
+        error: function(result){
+          console.log('error=', result)
+        }
+
+      });
+  }
+},
+
+songsListModule : {
+  showSongs : function(result){
+  console.log(result)};
+},
